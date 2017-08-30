e2fe501bc757ff1d2b6fc7d52000a1eb786d9889
Daniel Stockbridge <danielstockbridge@Daniels-MacBook-Pro-2.local>
2017-08-30 09:55:35 -0600

    Wed

--- program.py
+++ program.py
@@ -5,14 +5,11 @@ design = input('Enter the design name: ')
 company = input('Enter the company name: ')
 mall = input('Enter the mall name: ')
 
+# Print info to check for typos
+print('\nPlease check input information below for accuracy:')
 print('Design: ', design,)
 print('Company: ', company)
-print('Mall: ', mall)
-
-# Set string variables
-var1 = design
-var2 = company
-var3 = mall
+print('Mall: ', mall, "\n")
 
 # EDIT THESE WITH YOUR PATH TO /CoreWebSites/
 # Set directory locations
@@ -21,33 +18,29 @@ ddirectory = "/Users/danielstockbridge/Documents/PW/CoreWebSites/universal/"+ de
 udirectory = "/Users/danielstockbridge/Documents/PW/CoreWebSites/universal/universal_base/vars"
 
 # Print Directories - Purely for Debugging - Comment out if not needed
-print('Mall Dir: ', mdirectory)
-print('Design Dir: ', ddirectory)
-print('Universal Dir: ', udirectory)
-
-# Continue Question
-continue1 = input('Do you want to continue to diff the files?? (Y or N) ')
+# print('Mall Dir: ', mdirectory)
+# print('Design Dir: ', ddirectory)
+# print('Universal Dir: ', udirectory, "\n")
 
 # Diff Process
 folder_univ = set(os.listdir(udirectory))
 folder_mall = set(os.listdir(mdirectory))
 missing_from_mall = folder_univ - folder_mall
 
-# CMD
-cmd = "cd %s && grunt server --design=%s --company=%s --mall=%s"%(ddirectory,var1,var2,var3)
+# Grunt command
+cmd = "cd %s && grunt server --design=%s --company=%s --mall=%s"%(ddirectory,design,company,mall)
 
 # Run command
-if continue1 == 'Y':
-	# print_diff_files(dcmp)
-	print(missing_from_mall)
-	continue2 = input('Do you want to continue to compile the files?? (Y or N) ')
-	if continue2 == 'Y':
-		os.system(cmd)
-	else:
-		print('Exiting after cmp but before compile')
-		sys.exit()
+# print_diff_files
+print("\n".join(missing_from_mall))
+
+# Post-diff continue check
+continue2 = input('Do you want to continue to compile the files?? (Y or N) ')
+if continue2 == 'Y':
+	os.system(cmd)
 else:
-	print('Exiting before cmp and compile')
+	print('Exiting after diff but before compile')
 	sys.exit()
+
 
 print(file=sys.stderr)
